f3ae47e81052cc889083c624476581a4ec90f5b7
Daniel Kilgallon <danielkilgallon2@gmail.com>
2022-01-15 11:26:58 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-minecraft_wiki
+# Minecraft Wiki
+
+## generate-html.py
+This script loads the html file found at `template/template.html` and the json file `item-list.json`, goes through the html file and replaces keywords with their json counterpart ex. `{{minecraft_title}}` -> `Stable Ender Pearl`
+
+Then it writes the new, filled html file into the `generated-html` folder to be committed back into the repo.
--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -6,11 +6,11 @@ import re
 import json
 
 # Create the output folder if it does not already exist
-def generate_output_folder() -> None:
+def create_output_folder() -> None:
     if not os.path.isdir(constants.OUTPUT_DIR):
         os.mkdir(constants.OUTPUT_DIR)
 
-def generate_image_tag(output, image_paths) -> str:
+def replace_images(output, image_paths) -> str:
     print("Generating image tags...")
     for image_path in image_paths:
         re.sub(constants.IMAGE_REPLACE, image_path, output)
@@ -18,7 +18,6 @@ def generate_image_tag(output, image_paths) -> str:
 
 # Main Function
 def main() -> None:
-    # Generate an SVG badge with summary statistics
     with open(constants.TEMPLATE_DIR+"/template.html", "r") as f:
         output = f.read()
 
@@ -26,13 +25,13 @@ def main() -> None:
         item_list_json = j.read()
     item_list = json.loads(item_list_json)
 
-    generate_output_folder()
+    create_output_folder()
 
     for item in item_list:
         output = re.sub(constants.TITLE_REPLACE, item['title'], output)
         output = re.sub(constants.DESC_REPLACE, item['description'], output)
 
-        output = generate_image_tag(output, item['image_paths'])
+        output = replace_images(output, item['image_paths'])
         with open(constants.OUTPUT_DIR+"/generated.html", "w") as f:
             f.write(output)
 
