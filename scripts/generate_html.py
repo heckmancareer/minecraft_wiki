d0ac08c62752d9e4ca8030cc4283c8d837844c42
Daniel Kilgallon <danielkilgallon2@gmail.com>
2022-01-22 09:40:15 -0500

    fixing script

--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -18,22 +18,23 @@ def replace_images(output, image_paths) -> str:
 
 # Main Function
 def main() -> None:
-    with open(constants.TEMPLATE_DIR+"/template.html", "r") as f:
-        output = f.read()
+    with open(constants.TEMPLATE_DIR+"/template.html", "r") as template_file:
+        template = template_file.read()
+        
+        with open("item-list.json", "r") as json_file:
+            item_list_json = json_file.read()
+        item_list = json.loads(item_list_json)
 
-    with open("item-list.json", "r") as j:
-        item_list_json = j.read()
-    item_list = json.loads(item_list_json)
+        create_output_folder()
 
-    create_output_folder()
+        for item in item_list:
+            output = str(template)
+            output = re.sub(constants.TITLE_REPLACE, item['title'], output)
+            output = re.sub(constants.DESC_REPLACE, item['description'], output)
 
-    for item in item_list:
-        output = re.sub(constants.TITLE_REPLACE, item['title'], output)
-        output = re.sub(constants.DESC_REPLACE, item['description'], output)
-
-        output = replace_images(output, item['image_paths'])
-        with open(constants.OUTPUT_DIR+"/"+item['title'] + ".html", "w") as f:
-            f.write(output)
+            output = replace_images(output, item['image_paths'])
+            with open(constants.OUTPUT_DIR+"/"+item['title'] + ".html", "w") as output_file:
+                output_file.write(output)
 
 if __name__ == "__main__":
     main()
