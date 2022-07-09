6b22c12e8668ad4319dc8dbb7446424082070b73
Nick <heckmancareer@vivaldi.net>
2022-07-09 09:20:27 -0400

    Created sidebar generating function

--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -4,6 +4,7 @@ import constants
 import os
 import re
 import json
+from bs4 import BeautifulSoup
 
 # Create the output folder if it does not already exist
 def create_output_folder() -> None:
@@ -16,15 +17,38 @@ def replace_images(output, image_paths) -> str:
         re.sub(constants.IMAGE_REPLACE, image_path, output)
     return output
 
+def construct_sidebar(item_list_json) -> None:
+    with open("../" + constants.TEMPLATE_DIR+"/sidebar.html", "r") as sidebar_file: 
+        soup = BeautifulSoup(sidebar_file, 'html.parser')
+        item_list = json.loads(item_list_json)
+        item_headers = soup.find_all("div", class_="sidebar-header-item")
+
+        for item in item_list:
+            print(item['category'])
+            for header in item_headers:
+                if item['category'] == header.text.strip():
+                    newtag = BeautifulSoup('<div class="sidebar-line-item"></div>', 'html.parser')
+                    itemlink = BeautifulSoup('<a href="./' + item['title'] + '.html"></a>', 'html.parser')
+                    itemlink.a.append(item['title'])
+                    newtag.div.append(itemlink)
+                    headeritemidentifier = item['category'].lower() + "-header"
+                    soup.find("div", {"id": headeritemidentifier}).insert_after(newtag)
+
+        with open("output1.html", "w") as file:
+            soup.prettify()
+            file.write(str(soup))
+    return
+
 # Main Function
 def main() -> None:
-    with open(constants.TEMPLATE_DIR+"/template.html", "r") as template_file:
+    with open("../"+constants.TEMPLATE_DIR+"/template.html", "r") as template_file:
         template = template_file.read()
         
-        with open("item-list.json", "r") as json_file:
+        with open("../item-list.json", "r") as json_file:
             item_list_json = json_file.read()
         item_list = json.loads(item_list_json)
 
+        construct_sidebar(item_list_json)
         create_output_folder()
 
         for item in item_list:
